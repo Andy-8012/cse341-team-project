030c1183353f0b40035836a1d7b7533d0338e3cb
Andy-8012 <andrewvriley@gmail.com>
2026-02-05 20:18:03 -0700

    updated server.js to require databse functions

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParse = require('body-parser')
+const mongodb = require('./data/database')
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -22,9 +23,9 @@ process.on('uncaughtException', (err, origin) => {
 });
 
 
-app.listen(port, () => {
-    console.log('Web Server is listening at port ' + (port));
-});
+//app.listen(port, () => {
+//    console.log('Web Server is listening at port ' + (port));
+//});
 
 mongodb.initDb((err) => {
     if (err) {
