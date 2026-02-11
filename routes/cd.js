0de6c0302bfdab58319c75016e3181675e9d6027
Andy-8012 <andrewvriley@gmail.com>
2026-02-10 21:31:49 -0700

    added authenticate.js and cdValidator to the CD route

--- routes/cd.js
+++ routes/cd.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 
 const cdController = require('../controllers/cd');
+const cdValidator = require('../middleware/cdValidator')
 
 router.get('/', cdController.getAll);
 
 router.get('/:id', cdController.getById);
 
-router.post('/', cdController.createCD)
+router.post('/', cdValidator.cdValidationRules(), cdValidator.validate, cdController.createCD)
 
-router.put('/:id', cdController.updateCD)
+router.put('/:id', cdValidator.cdValidationRules(), cdValidator.validate, cdController.updateCD)
 
 router.delete('/:id', cdController.deleteCD)
 
